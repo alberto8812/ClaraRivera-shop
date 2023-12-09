fbcead9ce4ec40a1cb842789bd2258c4e4c22ad1
alberto8812 <cvelascosaavedra@gmail.com>
2023-12-09 02:38:26 -0500

    implement server action home

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,36 +4,49 @@ import {   SlideShowImage } from '@/components'
 import { ProductCardHome } from '@/components/product/productCardhome/ProductCardHome'
 import Link from 'next/link'
 import { titleFont } from '../config/fonts'
+import { getHomeProductsWithImages, getPaginatedProductsWithImages } from '@/actions'
 
-export default function Home() {
+export default async function Home() {
+
+  const productHome= await getHomeProductsWithImages();
+  //const productPaginateDB= await getPaginatedProductsWithImages();
+
+
+
   return (
     <div className="">
      <SlideShowImage/>
      
      <div className='m-2 p-5 flex flex-col justify-cente items-center '>
       {
-        categoruProduct.map(category=>(
-          <div key={category.category} className=''>
+        productHome?.map(category=>{
+          if(category.product.length)
+          return(
+          <div key={category.name} className=''>
             <div className='flex flex-col justify-center items-center'> 
               <div className='flex justify-center m-2 '>
-                <span className={`${titleFont.className}  text-2xl text-[rgba(0,81,89,255)] font-bold p-4 border  border-[rgba(0,81,89,255)] rounded-3xl`}>{category.category}</span>
+                <span className={`${titleFont.className}  text-2xl text-[rgba(0,81,89,255)] font-bold p-4 border  border-[rgba(0,81,89,255)] rounded-3xl`}>{category.name}</span>
               </div>
               <div className=' w-80 justify-center flex  bg-gray-400 items-center' >
                 <hr className='bg-gray-400 h-1'/>
               </div>
             </div>
-            <div className=' grid grid-cols-1 sm:grid-cols-3  justify-evenly  items-end  p-5   gap-4 '>
+           
+        
+           <div className=' grid grid-cols-1 sm:grid-cols-3  justify-evenly  items-end  p-5   gap-4 '>
             {
-              category.product.map(product=>(
-                 //todo link pending 
+              category.product?.map(product=>(
+                
                   
                       <ProductCardHome product={product} key={product.slug}/>
                    
                    ))
                   }
-                  </div>
+             </div>
+             
           </div>
-        ))
+          )
+        })
       }
       
      </div>
