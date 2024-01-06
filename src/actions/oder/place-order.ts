ab74be499a79e7132f855cc1348b286bd2c06cb1
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-01-06 15:34:57 -0500

    log order direction, number order

--- src/actions/oder/place-order.ts
+++ src/actions/oder/place-order.ts
@@ -46,6 +46,8 @@ export const placeOrder = async (
 
         if (!product) throw new Error(`${item.productId} no existe-500`);
 
+        const subTotal=product.price * productQueantity;
+
         totals.subTotal += subTotal;
         totals.tax += subTotal * 0.15;
         totals.total += subTotal * 1.15;
@@ -54,11 +56,68 @@ export const placeOrder = async (
       },
       { subTotal: 0, tax: 0, total: 0 }
     );
+
+    //crear transaccion
+    const prismaTx = prisma.$transaction(async (tx) => {
+      //1. actualizar el stock de los productos
+
+      //2. crear la orden header
+
+      const order = await tx.order.create({
+        data: {
+          userId: userId,
+          itemsInorder: itemsInorder,
+          subTotal: subTotal,
+          tax: tax,
+          total: total,
+
+          OrderItem: {
+            createMany: {
+              data: productIds.map((p) => ({
+                quantity: p.quantity,
+                size: p.size === undefined ? "NA" : p.size,
+                ProductId: p.productId,
+                price:
+                  products.find((product) => product.id === p.productId)
+                    ?.price ?? 0,
+              })),
+            },
+          },
+        },
+      });
+
+      //validar si el price es cero  , lanzar un error 
+
+      //3. crear la direccion de la orden
+      const {country,...restAddress}=addrress;
+      console.log(restAddress)
+      const orderAddress= await tx.orderAddress.create({
+        data:{
+          firstName:restAddress.firstName,
+          lastName:restAddress.lastName,
+          address:restAddress.address,
+          optionalAddres:restAddress.optionalAddres,
+          postalCode:restAddress.postalCode,
+          city:restAddress.city,
+          phone:restAddress.phone,
+          countryId:country,
+          orderId:order.id
+        }
+      })
+      return {
+        order: order,
+        updateProduct: [],
+     //   orderAddress: orderAddress,
+      };
+
+     
+    });
+
   } catch (error) {
     console.log(error);
     return {
       ok: false,
-      message: "",
+      message: error,
     };
   }
 };
