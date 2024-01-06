a17fbbd03e547878502f7bbcd6e4e1b6244fe9a4
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-01-06 17:29:41 -0500

    clear cart

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -2,104 +2,105 @@ import { create } from "zustand";
 import { CartProduct } from "@/components/interfaces";
 import { persist } from "zustand/middleware";
 
-
 interface State {
-    cart:CartProduct[];
+  cart: CartProduct[];
 
-    getTotalItems:()=>number;
-    getSumaryInformation:() => {
-        subtotal: number;
-        tax: number;
-        total: number;
-        itemsInCart: number;
-    };
+  getTotalItems: () => number;
+  getSumaryInformation: () => {
+    subtotal: number;
+    tax: number;
+    total: number;
+    itemsInCart: number;
+  };
 
-    addProductToCard:(product:CartProduct)=>void;
+  addProductToCard: (product: CartProduct) => void;
 
-    updateProductQuantity:(product:CartProduct,quantity:number)=>void;
-    removeProduct:(product:CartProduct)=>void;
+  updateProductQuantity: (product: CartProduct, quantity: number) => void;
+  removeProduct: (product: CartProduct) => void;
 
-    
+  clearCart: () => void;
 }
 
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
 
+      addProductToCard: (product: CartProduct) => {
+        const { cart } = get();
 
+        //1.revisar si el producto existe en el carrito con la talla seleccionada
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+        //2. se que el producto existe por tallas.. incrementar cantidad
+        const updatedCartProduct = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, queantity: item.quantity + product.quantity };
+          }
+          return item;
+        });
 
-export const useCartStore=create<State>()(
+        set({ cart: updatedCartProduct });
+      },
 
-    persist(
-        (set,get)=>({
-            cart:[],
-     
-            addProductToCard:(product:CartProduct)=>{
-             const {cart}=get();
-     
-             //1.revisar si el producto existe en el carrito con la talla seleccionada
-             const productInCart=cart.some(
-                 (item)=>(item.id===product.id && item.size===product.size)
-             );
-             if(!productInCart){
-                 set({cart:[...cart,product]})
-                 return;
-             }
-             //2. se que el producto existe por tallas.. incrementar cantidad
-              const updatedCartProduct=cart.map((item)=>{
-                 
-                 if(item.id===product.id && item.size===product.size){
-                     return {...item,queantity:item.quantity+product.quantity}
-                 }
-                 return item;
-              });
-     
-     
-              set({cart:updatedCartProduct});
-         
-            },
+      getTotalItems: () => {
+        const { cart } = get();
 
-            getTotalItems:()=>{
-                const {cart}=get();
+        return cart.reduce((total, item) => total + item.quantity, 0);
+      },
 
-                return cart.reduce((total,item)=>total+item.quantity,0);
-            },
+      getSumaryInformation: () => {
+        const { cart } = get();
+        const subtotal = cart.reduce(
+          (subtotal, producto) => producto.quantity * producto.price + subtotal,
+          0
+        );
+        const tax = subtotal * 0.15;
+        const total = subtotal + tax;
+        const itemsInCart = cart.reduce(
+          (total, item) => total + item.quantity,
+          0
+        );
 
-            getSumaryInformation:()=>{
-                const {cart}=get();
-                const subtotal= cart.reduce((subtotal,producto)=>(producto.quantity*producto.price)+subtotal,0);
-                const tax=subtotal*0.15;
-                const total=subtotal+tax;
-                const itemsInCart=cart.reduce((total,item)=>total+item.quantity,0);
+        return {
+          subtotal,
+          tax,
+          total,
+          itemsInCart,
+        };
+      },
 
-                return{
-                    subtotal,tax,total,itemsInCart
-                }
-            },
+      updateProductQuantity: (product: CartProduct, quantity: number) => {
+        const { cart } = get();
 
-            updateProductQuantity:(product:CartProduct,quantity:number)=>{
-                const {cart}=get();
-                    
-                const productfilter=cart.map(item=>{
-                    if(item.id===product.id && item.size===product.size){
-                        return {...item,quantity:quantity}
-                    }
-                    return item
-                });
+        const productfilter = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: quantity };
+          }
+          return item;
+        });
 
+        set({ cart: productfilter });
+      },
 
-                set({cart:productfilter});
+      removeProduct: (product: CartProduct) => {
+        const { cart } = get();
+        const updateCartProduct = cart.filter(
+          (item) => product.id !== item.id || item.size !== product.size
+        );
+        set({ cart: updateCartProduct });
+      },
 
+      clearCart: () => {
+        set({ cart: [] });
+      },
+    }),
 
-
-            },
-
-            removeProduct:(product:CartProduct)=>{
-                const {cart}=get();
-                const updateCartProduct=cart.filter(item=>(product.id!==item.id || item.size!==product.size));
-                set({cart:updateCartProduct});
-            },
-     
-         })
-
-        ,
-       { name:'shopping-cart'}
-    )
-)
+    { name: "shopping-cart" }
+  )
+);
